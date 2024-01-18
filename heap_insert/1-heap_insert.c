3eb5952aa41fb601aef6bacdf98dbfadda29b0e7
SimonDesc <pro.descombes@gmail.com>
2024-01-18 12:19:19 +0100

    doc: betty documentation

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * insert_at_first_open_position inserts a new node at the first available
+ * position in a binary heap.
+ *
+ * @current: A pointer to the current node in the heap where the new
+ * node will be inserted.
+ * @new_node: A pointer to the new node that needs to be inserted into
+ * the heap.
+ * @level: The level parameter represents the level at which a node can
+ * be inserted in the heap.
+ * It is used to determine the position of the new node in the heap.
+ * @done: The "done" parameter is a pointer to an integer variable that is
+ * used to indicate
+ * whether the insertion operation has been completed or not.
+ * It is initially set to 0 and will be set
+ * to 1 once the new node has been inserted into the heap.
+ * This allows the function to stop the
+ * recursive
+ *
+ * Return: nothing (void).
+ */
 static void insert_at_first_open_position(heap_t *current, heap_t *new_node,
 										  int level, int *done)
 {
@@ -43,6 +64,13 @@ static void insert_at_first_open_position(heap_t *current, heap_t *new_node,
 	}
 }
 
+/**
+ * tree_height - calculates the height of a binary tree.
+ *
+ * @root: The root parameter is a pointer to the root node of a binary tree.
+ *
+ * Return: the height of the tree.
+ */
 static int tree_height(heap_t *root)
 {
 	if (root == NULL)
@@ -63,6 +91,15 @@ static int tree_height(heap_t *root)
 	}
 }
 
+/**
+ * sift_up - a new node in a heap by swapping it with
+ * its parent if the parent's value is
+ * smaller.
+ *
+ * @new_node: A pointer to the newly inserted node in the heap.
+ *
+ * Return: a pointer to a heap node.
+ */
 heap_t *sift_up(heap_t *new_node)
 {
 	heap_t *current = new_node;
@@ -92,6 +129,19 @@ heap_t *sift_up(heap_t *new_node)
 	return (node_with_inserted_value);
 }
 
+/**
+ * heap_insert - inserts a new node with a given value into
+ * a binary heap and ensures that
+ * the heap property is maintained.
+ *
+ * @root: A double pointer to the root of the heap.
+ * This allows us to modify the root pointer if
+ * necessary.
+ * @value: The value to be inserted into the heap.
+ *
+ * Return: The function `heap_insert` returns a pointer to a `heap_t`
+ * structure.
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *new;
