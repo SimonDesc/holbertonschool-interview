265bde12a41d96eee922dadb1b2ec85d16112960
SimonDesc <pro.descombes@gmail.com>
2024-01-18 11:44:54 +0100

    feat: heap insert algorithm

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,135 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+static void insert_at_first_open_position(heap_t *current, heap_t *new_node, int level, int *done)
+{
+	if (current == NULL || *done)
+	{
+		return;
+	}
+
+	/* Si nous sommes au niveau où un nœud peut être inséré. */
+	if (level == 1)
+	{
+		/* Vérifier si l'enfant gauche est disponible. */
+		if (!current->left)
+		{
+			current->left = new_node;
+			new_node->parent = current;
+			*done = 1;
+		}
+		/* Sinon, vérifier si l'enfant droit est disponible. */
+		else if (!current->right)
+		{
+			current->right = new_node;
+			new_node->parent = current;
+			*done = 1;
+		}
+		else
+		{
+			/* Les deux enfants existent déjà */
+			/* La fonction retourne pour chercher une position dans un autre nœud.*/
+			return;
+		}
+	}
+
+	else if (level > 1)
+	{
+		/* On descend dans les branches de l'arbre */
+		insert_at_first_open_position(current->left, new_node, level - 1, done);
+		insert_at_first_open_position(current->right, new_node, level - 1, done);
+	}
+}
+
+static int tree_height(heap_t *root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+
+	int left_height = tree_height(root->left);
+	int right_height = tree_height(root->right);
+
+	if (left_height > right_height)
+	{
+		return left_height + 1;
+	}
+	else
+	{
+		return right_height + 1;
+	}
+}
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *new;
+
+	if (!root)
+	{
+		return NULL;
+	}
+
+	new = malloc(sizeof(heap_t));
+	if (new == NULL)
+	{
+		return NULL;
+	}
+
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = NULL;
+
+	/* Si l'arbre est vide, le nouveau nœud devient la racine.*/
+	if (!*root)
+	{
+		*root = new;
+	}
+	/* Sinon, trouver la position pour le nouvel élément. */
+	else
+	{
+		int done = 0;
+		/* On calcul la hauteur de l'arbre */
+		int height = tree_height(*root);
+		for (int level = 1; level <= height; ++level)
+		{
+			/*  */
+			insert_at_first_open_position(*root, new, level, &done);
+			if (done)
+			{
+				break;
+			}
+		}
+		/* Si le nœud n'a pas été inséré, l'insérer au niveau suivant.*/
+		if (!done)
+		{
+			insert_at_first_open_position(*root, new, height + 1, &done);
+		}
+	}
+
+	/* Réorganisation du tas (heapify up). */
+	heap_t *current = new;
+	while (current->parent)
+	{
+		heap_t *parent = current->parent;
+		/* Si le nœud courant est plus grand que son parent, échanger leurs valeurs. */
+		if (parent->n < current->n)
+		{
+			int temp = parent->n;
+			parent->n = current->n;
+			current->n = temp;
+
+			/* Continuer à remonter dans l'arbre.*/
+			current = parent;
+		}
+		else
+		{
+			/* Si le parent est plus grand, le tas est valide.*/
+			break;
+		}
+	}
+
+	/* Retourner le nouveau nœud inséré.*/
+	return new;
+}
