f66fb5c50f1a488404ca1ccc11ad57bd17f77a74
SimonDesc <pro.descombes@gmail.com>
2024-04-26 10:25:08 +0200

    feat: first commit - STARWARS API

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv[2]) {
+	const url = `https://swapi-api.hbtn.io/api/films/` + process.argv[2];
+	request({ url, json: true }, function (error, response, body) {
+		for (let i = 0; i < body.characters.length; i++) {
+			let urlCharacters = body.characters[i].toString();
+			request({ url: urlCharacters, json: true }, function (error, response, body) {
+				console.log(body.name)
+			})
+		}
+	});
+}
+else {
+	console.error("error: Please give an movie id - example : ./0-starwars_characters.js <1> ");
+}
