665407e6fb72bddfe0e843f2ff9535d326344d2c
SimonDesc <pro.descombes@gmail.com>
2024-04-26 10:33:37 +0200

    fix: promise for retrieving the characters in the same order

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,17 +2,36 @@
 
 const request = require('request');
 
-if (process.argv[2]) {
-	const url = `https://swapi-api.hbtn.io/api/films/` + process.argv[2];
-	request({url: url, json: true }, function (error, response, body) {
-		for (let i = 0; i < body.characters.length; i++) {
-			let urlCharacters = body.characters[i].toString();
-			request({ url: urlCharacters, json: true }, function (error, response, body) {
-				console.log(body.name)
-			})
-		}
+function fetch(url) {
+	return new Promise((resolve, reject) => {
+		request({ url: url, json: true }, (error, response, body) => {
+			if (error) {
+				return reject(error);
+			}
+			resolve(body);
+		});
 	});
 }
-else {
-	console.error("error: Please give an movie id - example : ./0-starwars_characters.js <1> ");
+
+
+if (process.argv[2]) {
+	const url = `https://swapi-api.hbtn.io/api/films/` + process.argv[2];
+	fetch(url).then(body => {
+		const characterPromises = body.characters.map(characterUrl => fetch(characterUrl));
+
+		Promise.all(characterPromises)
+			.then(characters => {
+				characters.forEach(character => {
+					console.log(character.name); // Afficher le nom du personnage
+				});
+			})
+			.catch(error => {
+				console.error('Error fetching characters:', error);
+			});
+	})
+		.catch(error => {
+			console.error('Error fetching film:', error);
+		});
+} else {
+	console.error("Error: Please provide a movie id - example: ./0-starwars_characters.js <1>");
 }
