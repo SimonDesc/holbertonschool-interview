10ac82f2af58e821a6acd690043c9a50cb478a0f
SimonDesc <pro.descombes@gmail.com>
2024-08-19 10:18:07 +0200

    test

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -2,24 +2,28 @@
 """ Module for Pascal's triangle """
 
 
-def pascal_triangle(numRows):
-    """ Returns list of lists of ints representing Pascal triangle of n """
+def pascal_triangle(n):
+    """ Returns list of lists of ints representing Pascal’s triangle of n """
 
-    result = []
-    if numRows == 0:
-        return result
+    if n <= 0:
+        return []
 
-    first_row = [1]
-    result.append(first_row)
+    pascal = [[1]]
+    if n == 1:
+        return pascal
 
-    for i in range(1, numRows):
-        prev_row = result[i - 1]
-        current_row = [1]
-
-        for j in range(1, i):
-            current_row.append(prev_row[j - 1] + prev_row[j])
-
-        current_row.append(1)
-        result.append(current_row)
-
-    return result
+    for i in range(1, n):
+        left = -1
+        right = 0
+        in_pas = []
+        for j in range(i+1):
+            num = 0
+            if left > -1:
+                num += pascal[i - 1][left]
+            if right < i:
+                num += pascal[i - 1][right]
+            left += 1
+            right += 1
+            in_pas.append(num)
+        pascal.append(in_pas)
+    return pascal
