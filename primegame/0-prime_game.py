086aa8949163d2853ba4abe521771dee9980d3e4
SimonDesc <pro.descombes@gmail.com>
2024-09-11 16:23:12 +0200

    feat: prime game initial commit

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,40 @@
+def isWinner(x, nums):
+    def sieve_of_eratosthenes(n):
+        primes = [True] * (n + 1)
+        primes[0] = primes[1] = False
+        for i in range(2, int(n**0.5) + 1):
+            if primes[i]:
+                for j in range(i*i, n + 1, i):
+                    primes[j] = False
+        return primes
+
+    def play_game(n):
+        primes = sieve_of_eratosthenes(n)
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            if primes[i]:
+                dp[i] = dp[i-1] + 1
+            else:
+                dp[i] = dp[i-1]
+        return dp[n] % 2 == 1
+
+    if not nums or x <= 0:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n <= 1:
+            ben_wins += 1
+        elif play_game(n):
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
