d7ef020ccdd23ec1a335bc672496d714bfd9e076
SimonDesc <pro.descombes@gmail.com>
2024-09-11 16:28:12 +0200

    fix: still trying to pass the checker

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -1,56 +1,49 @@
 #!/usr/bin/python3
-'''
-This module contains the solution for the Prime Game problem.
-'''
+
+""" Prime Game """
+
+
+def check_prime(n):
+    """ Check if n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
+
+
+def add_prime(n, primes):
+    """ Add prime to list """
+    last_prime = primes[-1]
+    if n > last_prime:
+        for i in range(last_prime + 1, n + 1):
+            if check_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
+
 
 def isWinner(x, nums):
-    '''
-    Determine the winner of multiple rounds of the Prime Game.
+    """ x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None """
 
-    Args:
-        x (int): The number of rounds to play.
-        nums (list of int): A list of n values for each round.
+    score = {"Maria": 0, "Ben": 0}
+    primes = [0, 0, 2]
+    add_prime(max(nums), primes)
 
-    Returns:
-        str or None: The name of the player who won the most rounds
-                     ("Maria" or "Ben"), or None if it's a tie.
-    '''
-    def sieve_of_eratosthenes(n):
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        for i in range(2, int(n**0.5) + 1):
-            if primes[i]:
-                for j in range(i*i, n + 1, i):
-                    primes[j] = False
-        return primes
-
-    def play_game(n):
-        primes = sieve_of_eratosthenes(n)
-        dp = [0] * (n + 1)
-        for i in range(2, n + 1):
-            if primes[i]:
-                dp[i] = dp[i-1] + 1
-            else:
-                dp[i] = dp[i-1]
-        return dp[n] % 2 == 1
-
-    if not nums or x <= 0:
-        return None
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if n <= 1:
-            ben_wins += 1
-        elif play_game(n):
-            maria_wins += 1
+    for round in range(x):
+        _sum = sum((i != 0 and i <= nums[round])
+                   for i in primes[:nums[round] + 1])
+        if (_sum % 2):
+            winner = "Maria"
         else:
-            ben_wins += 1
+            winner = "Ben"
+        if winner:
+            score[winner] += 1
 
-    if maria_wins > ben_wins:
+    if score["Maria"] > score["Ben"]:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif score["Ben"] > score["Maria"]:
         return "Ben"
-    else:
-        return None
+
+    return None
